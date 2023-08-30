7aa094884ae8f29c609fca5828a126d86fffda05
Artyom Sukiasyan <artiom.suqiasyan1401@gmail.com>
2023-08-30 22:27:26 +0400

    feat: add getDiagValues helper

---
+++ src/helpers/getDiagValues.ts
@@ -0,0 +1,19 @@
+export default function getDiagValues(arg: number, cells: number[]) {
+  if (arg !== 1 && arg !== 0) {
+    return [];
+  }
+
+  const newSells = [];
+
+  if (arg === 0) {
+    newSells.push(cells[0]);
+    newSells.push(cells[4]);
+    newSells.push(cells[8]);
+  } else {
+    newSells.push(cells[2]);
+    newSells.push(cells[4]);
+    newSells.push(cells[6]);
+  }
+
+  return newSells;
+}
