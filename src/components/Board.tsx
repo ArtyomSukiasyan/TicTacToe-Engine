10f7183064aaa4df036c71985fea41d5ebf3f2bf
Artyom Sukiasyan <artiom.suqiasyan1401@gmail.com>
2023-08-30 22:31:28 +0400

    feat: add Board component

---
+++ src/components/Board.tsx
@@ -0,0 +1,25 @@
+interface IBoard {
+  cells: string[];
+  values: string[];
+  cellClicked: Function;
+}
+
+export default function Board({ cells, values, cellClicked }: IBoard) {
+  return (
+    <>
+      <h1>Tic-Tac-Toe</h1>
+      <div className="board">
+        {cells.map((_, idx: number) => (
+          <div
+            key={idx}
+            id={idx.toString()}
+            onClick={(event: any) => cellClicked(event.target.id, cells)}
+            className="fixed"
+          > 
+            {values[idx]}
+          </div>
+        ))}
+      </div>
+    </>
+  );
+}
