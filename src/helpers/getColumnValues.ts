55f5c5fd301d294977d47f6f49cfcfe75423b31b
Artyom Sukiasyan <artiom.suqiasyan1401@gmail.com>
2023-08-30 22:26:59 +0400

    feat: add getColumnValues helper

---
+++ src/helpers/getColumnValues.ts
@@ -0,0 +1,13 @@
+export default function getColumnValues(index: number, cells: number[]) {
+  if (index !== 0 && index !== 1 && index !== 2) {
+    return [];
+  }
+
+  const column = [];
+
+  for (let i = index; i < cells.length; i += 3) {
+    column.push(cells[i]);
+  }
+
+  return column;
+}
