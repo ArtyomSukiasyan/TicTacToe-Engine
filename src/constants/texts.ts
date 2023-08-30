450aaac8eddb0d7d625543c170c6f995e419cf3c
Artyom Sukiasyan <artiom.suqiasyan1401@gmail.com>
2023-08-30 22:25:43 +0400

    feat: add texts constants

---
+++ src/constants/texts.ts
@@ -0,0 +1,11 @@
+export const youWin = "Congratulations, you won!";
+
+export const draw = "It's a draw!";
+
+export const computerWins = "Computer wins!";
+
+export const empty = "";
+
+export const X = "X";
+
+export const O = "O"
