efcb879844e265186a152dc1a3262b37a9e2b6ff
Artyom Sukiasyan <artiom.suqiasyan1401@gmail.com>
2023-08-31 13:57:35 +0400

    feat: refactor Score component

--- src/components/Score.tsx
+++ src/components/Score.tsx
@@ -1,18 +1,36 @@
 import { IScore } from "../models/score";
 
+const computer = "Computer";
+const draws = "Draws";
+const player = "Player";
+
 export default function Score({ score }: { score: IScore }) {
   return (
     <section>
-      <div className="score-container">
-        <span>Computer</span>
-        <span>Draws</span>
-        <span>Player</span>
-      </div>
-      <div className="score-container">
-        <span>{score.computer}</span>
-        <span>{score.draws}</span>
-        <span>{score.player}</span>
-      </div>
+      <Players />
+      <Points score={score} />
     </section>
+  );
+}
+
+function Players() {
+  const players = [player, draws, computer];
+
+  return (
+    <div className="score-container">
+      {players.map((el) => (
+        <span key={el}>{el}</span>
+      ))}
+    </div>
+  );
+}
+
+function Points({ score }: { score: IScore }) {
+  return (
+    <div className="score-container">
+      {Object.values(score).map((el, idx) => (
+        <span key={idx}>{el}</span>
+      ))}
+    </div>
   );
 }
