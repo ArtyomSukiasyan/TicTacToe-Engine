1c091d7b0ad4c2ef2403f310f1f7d26ad7d0a598
Artyom Sukiasyan <artiom.suqiasyan1401@gmail.com>
2023-09-01 00:28:46 +0400

    feat: update sumArray

--- src/helpers/sumArray.ts
+++ src/helpers/sumArray.ts
@@ -4,8 +4,16 @@ export default function sumArray(array: string[]) {
   let sum = 0;
 
   for (let i = 0; i < array.length; i++) {
-    const value = array[i] === X ? 1 : array[i] === O ? 3 : 0;
-    sum += value;
+    switch (array[i]) {
+      case X:
+        sum += 1;
+        break;
+      case O:
+        sum += 3;
+        break;
+      default:
+        break;
+    }
   }
 
   return sum;
